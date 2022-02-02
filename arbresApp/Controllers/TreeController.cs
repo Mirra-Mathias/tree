5d1ecb0fc155230ca31f7811df35dc24072b2ebd
Mirra Mathias <52259162+Mirra-Mathias@users.noreply.github.com>
2022-02-02 14:35:02 +0100

    first commit

---
+++ arbresApp/Controllers/TreeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using Microsoft.AspNetCore.Mvc;
+
+public class App
+{
+    public string link { get; set; }
+}
+
+namespace arbresApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TreeController : Controller
+    {
+
+        private Tree[] trees;
+
+        public TreeController()
+        {
+            string json = System.IO.File.ReadAllText(@"Data\app.json");
+            App jso = JsonSerializer.Deserialize<App>(json);
+            var arbrestring = new WebClient().DownloadString(jso.link);
+            Record rec = JsonSerializer.Deserialize<Record>(arbrestring);
+            trees = rec.records;
+        }
+
+        [HttpGet]
+        public IEnumerable<Tree> Get()
+        {
+            IEnumerable<Tree> trees = this.trees;
+            return trees;
+        }
+        
+        public IEnumerable<Tree> Get(int id)
+        {
+            IEnumerable<Tree> trees = this.trees;
+            return trees;
+        }
+    }
+}
---
+++ arbresApp/Tree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace arbresApp
+{
+    public class Field
+    {
+        public string libellefrancais { get; set; }
+        public double circonferenceencm { get; set; }
+        public double hauteurenm { get; set; }
+        public string stadedeveloppement { get; set; }
+        public DateTime dateplantation { get; set; }
+    }
+    public class Tree
+    {
+        public string datasetid { get; set; }
+        public string recordid{ get; set; }
+        
+        public Field fields { get; set; }
+        
+        public string domanialite { get; set; }
+    }
+
+    public class Record
+    {
+        public Tree[] records { get; set; }
+    }
+}
